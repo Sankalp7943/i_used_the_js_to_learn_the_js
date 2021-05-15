d63a7d0dace40cdd5a4eec5d0ca90a069962d73d
Sankalp Sharma <sharmasankalp@outlook.com>
2021-05-15 19:00:23 +0530

    finished simulation code also

--- scripts/myscripts.js
+++ scripts/myscripts.js
@@ -203,9 +203,229 @@ const scopeEvents = function(e){
 document.querySelector("#scope").addEventListener("click", scopeEvents)
 
 
+const simulation = function(){
+    statements = [
+        "console.log('Start')",
+        "setTimeout(xxxxx)", //"setTimeout(function callBackTimeoutFunction(){console.log('CallBack setTimeout')}, 5000)",
+        "fetch(xxxx)",    //"fetch('XYZ API call').then(function callBackFunctionFetch(){console.log('API call')})",
+        "more code (More Code Output)....",
+        "console.log('End')"
+    ]
 
+    const start = function(){
+        document.querySelector("#simulationParent").innerHTML = `<div>
+        <h3 class="simulationHeading">
+            Event Loop Waiting for Call Stack to get Empty
+        </h3>
+    </div>
+    <div>
+        <h3 class="simulationHeading">
+            Call Stack
+        </h3>
+        <p id="simulationCallStack" class="simulationCalls"></p>
+    </div>
+    <div>
+        <h3 class="simulationHeading">
+            MicroTask Queue
+        </h3>
+        <p id="simulationmicrotaskQueue" class="simulationCalls"></p>
+    </div>
+    <div>
+        <h3 class="simulationHeading">
+            CallBack Queue
+        </h3>
+        <p id="simulationcallbackQueue" class="simulationCalls"></p>
+    </div>
+    <div>
+        <h3 class="simulationHeading">
+            Console Output
+        </h3>
+        <p id="simulationconsoleOutput" class="simulationCalls"></p>
+    </div>`
+    }
 
-//scope chain and shadowing //block scope etc etc
+    const end = function(){
+        document.querySelector("#simulationParent").innerHTML = ""
+    }
+
+    const reset = function(){
+        document.querySelector("#simulationCallStack").innerHTML = "Empty"
+        document.querySelector("#simulationmicrotaskQueue").innerHTML = "Empty"
+        document.querySelector("#simulationcallbackQueue").innerHTML = "Empty"
+        document.querySelector("#simulationconsoleOutput").innerHTML = "Empty"
+    }
+
+    const display = function(i){
+        switch(i){
+            case (0):{
+                document.querySelector("#simulationCallStack").innerHTML = statements[i]
+                document.querySelector("#simulationmicrotaskQueue").innerHTML = "Empty"
+                document.querySelector("#simulationcallbackQueue").innerHTML = "Empty"
+                document.querySelector("#simulationconsoleOutput").innerHTML = "Empty"
+                break
+            }
+            case(1):{
+                document.querySelector("#simulationCallStack").innerHTML = statements[i]
+                document.querySelector("#simulationmicrotaskQueue").innerHTML = "Empty"
+                document.querySelector("#simulationcallbackQueue").innerHTML = "Empty"
+                document.querySelector("#simulationconsoleOutput").innerHTML = "Start"
+                break
+            }
+            case(2):{
+                document.querySelector("#simulationCallStack").innerHTML = statements[i]
+                document.querySelector("#simulationmicrotaskQueue").innerHTML = "Empty"
+                document.querySelector("#simulationcallbackQueue").innerHTML = "Empty"
+                document.querySelector("#simulationconsoleOutput").innerHTML = "Start"
+                break
+            }
+            case(3):{
+                document.querySelector("#simulationCallStack").innerHTML = statements[i]
+                document.querySelector("#simulationmicrotaskQueue").innerHTML = "callBackFunctionFetch(){console.log('API call')}"
+                document.querySelector("#simulationcallbackQueue").innerHTML = "Empty"
+                document.querySelector("#simulationconsoleOutput").innerHTML = `Start`
+                break
+            }
+            case(4):{
+                document.querySelector("#simulationCallStack").innerHTML = statements[i]
+                document.querySelector("#simulationmicrotaskQueue").innerHTML = "callBackFunctionFetch(){console.log('API call')}"
+                document.querySelector("#simulationcallbackQueue").innerHTML = "Empty"
+                document.querySelector("#simulationconsoleOutput").innerHTML = `Start<br>More Code Output`
+                break
+            }
+            case(4):{
+                document.querySelector("#simulationCallStack").innerHTML = statements[i]
+                document.querySelector("#simulationmicrotaskQueue").innerHTML = "callBackFunctionFetch(){console.log('API call')}"
+                document.querySelector("#simulationcallbackQueue").innerHTML = "callBackTimeoutFunction(){console.log('CallBack setTimeout')}"
+                document.querySelector("#simulationconsoleOutput").innerHTML = `Start<br>More Code Output`
+                break
+            }
+            case(5):{
+                document.querySelector("#simulationCallStack").innerHTML = "Empty (Event loop was waiting for this)"
+                document.querySelector("#simulationmicrotaskQueue").innerHTML = "callBackFunctionFetch(){console.log('API call')}"
+                document.querySelector("#simulationcallbackQueue").innerHTML = "callBackTimeoutFunction(){console.log('CallBack setTimeout')}"
+                document.querySelector("#simulationconsoleOutput").innerHTML = `Start<br>More Code Output<br>End`
+                break
+            }
+            case(6):{
+                document.querySelector("#simulationCallStack").innerHTML = "callBackFunctionFetch(){console.log('API call')}"
+                document.querySelector("#simulationmicrotaskQueue").innerHTML = "Empty"
+                document.querySelector("#simulationcallbackQueue").innerHTML = "callBackTimeoutFunction(){console.log('CallBack setTimeout')}"
+                document.querySelector("#simulationconsoleOutput").innerHTML = `Start<br>More Code Output<br>End`
+                break
+            }
+            case(7):{
+                document.querySelector("#simulationCallStack").innerHTML = "callBackTimeoutFunction(){console.log('CallBack setTimeout')}"
+                document.querySelector("#simulationmicrotaskQueue").innerHTML = "Empty"
+                document.querySelector("#simulationcallbackQueue").innerHTML = "Empty"
+                document.querySelector("#simulationconsoleOutput").innerHTML = `Start<br>More Code Output<br>End<br>API call`
+                break
+            }
+            case(8):{
+                document.querySelector("#simulationCallStack").innerHTML = "Empty"
+                document.querySelector("#simulationmicrotaskQueue").innerHTML = "Empty"
+                document.querySelector("#simulationcallbackQueue").innerHTML = "Empty"
+                document.querySelector("#simulationconsoleOutput").innerHTML = `Start<br>More Code Output<br>End<br>API call<br>CallBack setTimeout`
+                break
+            }
+            default:{
+            }
+    	}
+        return
+    }
+
+    const update = async(i) => {
+        switch(i){
+            case (0):{
+                console.log("console.log('Start') pushed into call stack'")
+                break
+            }
+            case (1):{
+                console.log("SetTimeout is called with a call back, Browser API starts the timer for 5 seconds")
+                break
+            }
+            case (2):{
+                console.log("Fetch API from browser has been called to make a network call and a promise is made")
+                break
+            }
+            case (3):{
+                console.log("Response from a network all is recieved and added to MicroTask Queue while program moved forward")
+                break
+            }
+            case (4):{
+                console.log("Timer of 5seconds is finished and callback function is pushed to callBack Queue while more code is executed")
+                break
+            }
+            case (5):{
+                console.log("Program Ends and call stack gets free, which was constantly being checked by Event Loop")
+                break
+            }
+            case (6):{
+                console.log("Event loops fetch and put the task from MicroTask Queue to call stack (Priority: MicroTask Queue > CallBacks Queue)")
+                break
+            }
+            case (7):{
+                console.log("Event loops fetch and put the task from CallBack Queue as call stack gets empty and there no job in McroTask Queue")
+                break
+            }
+            case (8):{
+                console.log("Program Ends")
+                break
+            }
+            default:{
+            }
+        }
+        display(i)
+        return
+
+    }
+
+    const delay = ms => new Promise(res => setTimeout(res, ms))
+    const myDelay = async (interval) => {
+    await delay(interval)
+    return
+
+  }
+
+    const run = async() => {
+        start()
+        reset()
+
+        for (let i = 0; i<9; i++){
+            await update(i)
+            await myDelay(5000)
+
+        }
+        reset()
+        end()
+        eventListening = 1
+
+    }
+    run()
+}
+
+//also example of event delegation, will be proviging a specific example for it too
+const simulationEvents = function(e){
+    console.log("Simulation div clicked")
+    if (e.target.tagName === "BUTTON")
+        if (e.target.id === "simulationButton"){
+            if (eventListening){
+                eventListening = 0
+                simulation()
+            }
+        }
+        else if (e.target.id === "simulationLearn"){
+            link1 = "https://youtu.be/8zKuNo4ay8E"
+            window.open(link1, "_blank")
+        }
+        else{
+            clear()
+        }
+}
+eventListening = 1
+
+document.querySelector("#simulation").addEventListener("click", simulationEvents)
+
+//event loop and everything
 // closures
 //temporal deadzone
 //settimout and closures
